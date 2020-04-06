88145be17ccd2522b7533aed1d3fabd6333eb1a3
Alex <Alexander.Wallwork@infinityworks.com>
2020-04-06 11:00:53 +0100

    improved unittest

--- player.py
+++ player.py
@@ -8,14 +8,22 @@
 # Gender
 # Status (Playing/ Not)
 
-#Gary McTest is male, age 7, both-footed and currently able to play."
+# Gary McTest is male, age 7, both-footed and currently able to play."
 
-def player(name, age, foot, gender, player):
-    if player == False:
-        aval = "unable"
-    else:
-        aval = "able"
-    return f"{name} is {gender.lower()}, age {age}, {foot.lower()}-footed and currently {aval} to play."
+class Player:
+    def __init__(self, name, age, foot, gender, player):
+        self.name = name
+        self.age = age
+        self.foot = foot
+        self.gender = gender 
+        self.player = player 
+
+    def get_player_info(self):
+        if self.player == False:
+            aval = "unable"
+        else:
+            aval = "able"
+        return f"{self.name} is {self.gender.lower()}, age {self.age}, {self.foot.lower()}-footed and currently {aval} to play."
     
 
 # print(player("Gaz",65,"Right","Female",True))
--- test_file.py
+++ test_file.py
@@ -1,5 +1,5 @@
 import unittest.mock
-import player
+from player import Player
 
 
 class PlayerTests(unittest.TestCase):
@@ -7,15 +7,17 @@ class PlayerTests(unittest.TestCase):
     def test_player_unable_to_play(self):
         # Arrange
         expected_outcome = "Gary McTest is male, age 7, both-footed and currently unable to play."
+        testy = Player("Gary McTest", 7, "Both", "Male", False)
         # Act
-        actual_outcome = player.player("Gary McTest",7,"Both","Male",False)
+        actual_outcome = testy.get_player_info()
         # Assert
         self.assertEqual(expected_outcome,actual_outcome)
 
     def test_player_able_to_play(self):
         # Arrange
         expected_outcome = "Gary McTest is male, age 7, both-footed and currently able to play."
+        testy = Player("Gary McTest", 7, "Both", "Male", True)
         # Act
-        actual_outcome = player.player("Gary McTest",7,"Both","Male",True)
+        actual_outcome = testy.get_player_info()
         # Assert
         self.assertEqual(expected_outcome,actual_outcome)
