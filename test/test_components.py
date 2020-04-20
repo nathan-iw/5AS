acc0a197c167f8c5e62d333d0c825613908d97bd
Samuel Evans <samuelevans@samuels-mbp.home>
2020-04-20 12:06:21 +0100

    Removed the commented copy of process customers

--- test/test_components.py
+++ test/test_components.py
@@ -14,17 +14,3 @@ class ComponentTest(unittest.TestCase):
         #assert        
         assert actual == expected, f""" actual: {actual}
         expected: {expected}"""
-
-    # def process_customers(data):
-    # customer_list=[]
-    # for row in data:
-    #     if len(row) == 0 or row[0] == "Name": # check for empty rows or rows indicating table headers 
-    #         continue # if the above is the case skips that row 
-    #     try:
-    #         age = age_gen(row[1]) # try to generate an age from the DOB 
-    #     except ValueError: # if that fails gives a value error
-    #         continue # then skips that row
-    #     title, first_name, last_name = name_breaker(row[0]) # assigning the values in the tuple
-    #     customer_tuple = (title, first_name, last_name, age, card_masker(row[-1])) # making a new tuple with the age and masked ccn
-    #     customer_list.append(customer_tuple) # appending aforementioned tuple to the customer_list
-    # return customer_list # returns list 
